73f82fc05f76cb8bcb4856cc49642435d3af07fb
Walter406 <franpiu25s@gmail.com>
2021-11-29 01:31:57 -0600

    Se agregaron todas las canciones con su nombre y duracion, se modificaron los botones(hay que ver como hacer que funcionen los quicksand fonts), la barra de la playlist funciona correctamente, revisar el problema con las imagenes, se inicio a trabajar con el .js, y el video quedo en el minuto 37:14

---
+++ public/script.js
@@ -0,0 +1,4 @@
+const audio = document.querySelector('audio');
+const playPauseBtn = Document.querySelector('#play-pause');
+const nextBtn = Document.querySelector('#next');
+const preveBtn = Document.querySelector('#previous');
